9f493779f538de2be60d6304b4443ee6ea43ab71
Emanuel Heitlinger <emanuel.heitlinger@lua.rlp.de>
2025-04-01 12:25:01 +0200

    Ridom bioinfo pipeline evaluation

--- R/RIDOM_pipeline_evaluation.R
+++ R/RIDOM_pipeline_evaluation.R
@@ -1,6 +1,37 @@
-library(magrittr)
 
-RIDOM_comptable_re_evaluate <- function (old_folder, new_folder,
+#' RIDOM_comptable_re_evaluate
+#'
+#' @param old_folder A folder containing RIDOM comparison tables in csv format,
+#' these should be "old", e.g. from a previously run, changed pipeline or simply
+#' the old state of exports in a routin re-evaluation
+#'
+#' @param new_folder A second folder containing RIDOM comparison tables in csv
+#' format, these should be "new", e.g. from a novel changed pipeline or simply
+#' the new state of exports in a routine re-evaluation
+#'
+#'
+#' @param wanted_cols the columns that should be compared. Defaults to
+#' "Sample ID", "Perc. Good Targets", "Avg. Coverage (Assembled)", "ST"
+#' (all projects but EHEC), "ST Warwick" (project Produktiv_EHEC),
+#' "Complex Type", "N50 (Assembled)", "O Type" (project Produktiv_EHEC),
+#' "H Type" (project Produktiv_EHEC) and "Serovar" (project Produkiv_Salmonella)
+#'
+#' @param out_folder output folder for the comparison tables of old vs. new
+#'
+#' @return csv files are written into a folder: One file for each Assembly
+#' (SOP), EHEC, Salmonella, Listeria, MRSA and Legionella (each an own PRM),
+#' the corresponding dataframes are returned within a list.
+#'
+#' @export
+#'
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#' foo <- RIDOM_comptable_re_evaluate("O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_19/",
+#' "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_20/",
+#' "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/test/")
+
+RIDOM_comptable_re_evaluate <- function (old_folder, new_folder, out_folder,
                                          wanted_cols = c(
                                            "Sample ID",
                                            "Perc. Good Targets",
@@ -13,9 +44,11 @@ RIDOM_comptable_re_evaluate <- function (old_folder, new_folder,
   read_file_folder <- function (fo = folder) {
     files <- list.files(fo, pattern = "*.csv", full.names = TRUE)
     dfs <- lapply(files, function (x) {
-      readr::read_delim(x,  delim = ";",
-                        locale = readr::locale(decimal_mark = "."),
-                        show_col_types = FALSE)
+      suppressWarnings(
+        readr::read_delim(x,  delim = ";",
+                          locale = readr::locale(decimal_mark = "."),
+                          show_col_types = FALSE)
+      )
     })
     dfs <- lapply(dfs, function(x) x[colnames(x)%in%wanted_cols])
     names(dfs) <- basename(files)
@@ -23,13 +56,39 @@ RIDOM_comptable_re_evaluate <- function (old_folder, new_folder,
   }
   old_dfs <- read_file_folder(old_folder)
   new_dfs <- read_file_folder(new_folder)
-  list(old=old_dfs, new=new_dfs)
+
+  ass_stats <- combine_assembly_stats(old_dfs, new_dfs)
+  ehec_stats <- combine_ehec_stats(old_dfs, new_dfs)
+  salmonella_stats <- combine_salmonella_stats(old_dfs, new_dfs)
+  other_stats <- lapply(c("Legionella", "MRSA", "Listeria"), function(x) {
+    combine_general_stats(old_dfs, new_dfs, what = x)
+  })
+  names(other_stats) <- c("Legionella", "MRSA", "Listeria")
+  all_stats <- c(list(Assembly = ass_stats,
+                      EHEC = ehec_stats,
+                      Salmonella = salmonella_stats),
+                      other_stats)
+  message("Writing evaluation tables comparing old and new results to:")
+  for(i in seq_along(all_stats)){
+      path <- paste0(out_folder,  names(all_stats)[[i]], ".csv")
+      readr::write_csv2(all_stats[[i]], path)
+      message(path)
+    }
+  all_stats
 }
 
-foo <- RIDOM_comptable_re_evaluate("O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_19/",
-                                   "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_20/")
 
 
+#' combine_assembly_stats
+#'
+#' @param old The dataframes derived from old RIDOM comparison tables
+#' @param new The dataframes derived from new RIDOM comparison tables
+#'
+#' @return A dataframe showing assembly comparisons. The difference of the N50
+#' values of old vs. new assemblies are given percent.
+#'
+#' @importFrom magrittr %>%
+#'
 combine_assembly_stats <- function(old, new) {
   ass_list <- lapply(seq_along(old), function(i){
     comb <- old[[i]] %>%
@@ -41,6 +100,15 @@ combine_assembly_stats <- function(old, new) {
   do.call("rbind", ass_list)
 }
 
+#' combine_ehec_stats
+#'
+#' @param old The dataframes derived from old RIDOM comparison tables
+#' @param new The dataframes derived from new RIDOM comparison tables
+#'
+#' @return a dataframe with comparisons for EHEC
+#'
+#' @importFrom magrittr %>%
+#'
 combine_ehec_stats <- function(old, new) {
   oldEHEC <- old[[grep("EHEC", names(old))]] %>% tidyr::as_tibble()
   newEHEC <- new[[grep("EHEC", names(new))]] %>% tidyr::as_tibble()
@@ -58,6 +126,15 @@ combine_ehec_stats <- function(old, new) {
   comb
 }
 
+#' combine_salmonella_stats
+#'
+#' @param old The dataframes derived from old RIDOM comparison tables
+#' @param new The dataframes derived from new RIDOM comparison tables
+#'
+#' @return a dataframe with comparisons for Salmonella
+#'
+#' @importFrom magrittr %>%
+#'
 combine_salmonella_stats <- function(old, new) {
   oldSal <- old[[grep("Salmonella", names(old))]] %>% tidyr::as_tibble()
   newSal <- new[[grep("Salmonella", names(new))]] %>% tidyr::as_tibble()
@@ -75,6 +152,17 @@ combine_salmonella_stats <- function(old, new) {
 }
 
 
+#' combine_general_stats
+#'
+#' @param old The dataframes derived from old RIDOM comparison tables
+#' @param new The dataframes derived from new RIDOM comparison tables
+#'
+#' @param what either Listeria, MRSA or Legionella
+#'
+#' @return a data frame containg comparison statistics for what
+#'
+#' @importFrom magrittr %>%
+#'
 combine_general_stats <- function(old, new, what) {
   oldThis <- old[[grep(what, names(old))]] %>% tidyr::as_tibble()
   newThis <- new[[grep(what, names(new))]] %>% tidyr::as_tibble()
@@ -92,37 +180,23 @@ combine_general_stats <- function(old, new, what) {
 }
 
 
-
-Assembly <- combine_assembly_stats(foo[["old"]], foo[["new"]])
-
-EHEC <- combine_ehec_stats(foo[["old"]], foo[["new"]])
-
-Salmonella <- combine_salmonella_stats(foo[["old"]], foo[["new"]])
-
-Listeria <- combine_general_stats(foo[["old"]], foo[["new"]], "Listeria")
-
-MRSA <- combine_general_stats(foo[["old"]], foo[["new"]], "MRSA")
-
-Legionella <- combine_general_stats(foo[["old"]], foo[["new"]], "Legionella")
-
-library(readr)
-
-write_csv2(Assembly,
-           "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Assembly.csv")
-
-write_csv2(EHEC,
-           "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/EHEC.csv")
-
-write_csv2(Salmonella,
-           "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Salmonella.csv")
-
-write_csv2(Listeria,
-           "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Listeria.csv")
-
-write_csv2(MRSA,
-           "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/MRSA.csv")
-
-write_csv2(Legionella,
-           "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Legionella.csv")
-
-
+#
+# write_csv2(Assembly,
+#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Assembly.csv")
+#
+# write_csv2(EHEC,
+#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/EHEC.csv")
+#
+# write_csv2(Salmonella,
+#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Salmonella.csv")
+#
+# write_csv2(Listeria,
+#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Listeria.csv")
+#
+# write_csv2(MRSA,
+#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/MRSA.csv")
+#
+# write_csv2(Legionella,
+#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Legionella.csv")
+#
+#
