e205b3f8c5b7ad515827810640c26bd4ee9a945a
Emanuel Heitlinger <emanuel.heitlinger@lua.rlp.de>
2025-04-24 14:59:38 +0200

    Pipeline re-Evaluation vignette finished

--- R/RIDOM_pipeline_evaluation.R
+++ R/RIDOM_pipeline_evaluation.R
@@ -24,13 +24,17 @@
 #'
 #' @export
 #'
-#' @importFrom magrittr %>%
+#' @seealso \code{\link[=vignette]{vignette("QM_pipeline_evaluation")}}
 #'
 #' @examples
-#' foo <- RIDOM_comptable_re_evaluate("O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_19/",
-#' "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_20/",
-#' "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/test/")
-
+#'Re_Eval <- RIDOM_comptable_re_evaluate(
+#'old_folder =
+#'"O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_19/",
+#'new_folder =
+#'  "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/2025_03_20/",
+#'  out_folder =
+#'  "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/test/")
+#'
 RIDOM_comptable_re_evaluate <- function (old_folder, new_folder, out_folder,
                                          wanted_cols = c(
                                            "Sample ID",
@@ -41,21 +45,26 @@ RIDOM_comptable_re_evaluate <- function (old_folder, new_folder, out_folder,
                                            "N50 (Assembled)",
                                            "O Type", "H Type",
                                            "Serovar")){
-  read_file_folder <- function (fo = folder) {
-    files <- list.files(fo, pattern = "*.csv", full.names = TRUE)
+  read_file_folder <- function (path) {
+    message("Reading: ")
+    files <- list.files(path = path, pattern = "*.csv", full.names = TRUE)
     dfs <- lapply(files, function (x) {
-      suppressWarnings(
-        readr::read_delim(x,  delim = ";",
-                          locale = readr::locale(decimal_mark = "."),
-                          show_col_types = FALSE)
+      message(x)
+      suppressMessages(
+        suppressWarnings(
+          readr::read_delim(x, delim = ";",
+                            locale = readr::locale(decimal_mark = "."),
+                            show_col_types = FALSE,
+                            name_repair = "minimal")
+        )
       )
     })
     dfs <- lapply(dfs, function(x) x[colnames(x)%in%wanted_cols])
     names(dfs) <- basename(files)
     dfs
   }
-  old_dfs <- read_file_folder(old_folder)
-  new_dfs <- read_file_folder(new_folder)
+  old_dfs <- read_file_folder(path = old_folder)
+  new_dfs <- read_file_folder(path = new_folder)
 
   ass_stats <- combine_assembly_stats(old_dfs, new_dfs)
   ehec_stats <- combine_ehec_stats(old_dfs, new_dfs)
@@ -68,9 +77,19 @@ RIDOM_comptable_re_evaluate <- function (old_folder, new_folder, out_folder,
                       EHEC = ehec_stats,
                       Salmonella = salmonella_stats),
                       other_stats)
+  if(!dir.exists(out_folder)){
+    message("Ordner ", out_folder, " wird angelegt.")
+    dir.create(out_folder)
+  }
   message("Writing evaluation tables comparing old and new results to:")
   for(i in seq_along(all_stats)){
-      path <- paste0(out_folder,  names(all_stats)[[i]], ".csv")
+      path <- paste0(out_folder,  names(all_stats)[[i]], ".csv ")
+      if(file.exists(path)){
+        stop("Datei ", path, "ist bereits vorhanden! ",
+             "Soll die SELBE Evaluierung wirklich wiederholt werden? ",
+             "Lösche die Datei oder den Ordner für eine Wiederholung oder
+             speichere in einem neuen Ordner für eine NEUE Evaluierung!")
+      }
       readr::write_csv2(all_stats[[i]], path)
       message(path)
     }
@@ -93,9 +112,11 @@ combine_assembly_stats <- function(old, new) {
   ass_list <- lapply(seq_along(old), function(i){
     comb <- old[[i]] %>%
       dplyr::inner_join(new[[i]], by = "Sample ID",  suffix = c("_old", "_new")) %>%
-      dplyr::select(`Sample ID`, `N50 (Assembled)_old`, `N50 (Assembled)_new`) %>%
-      dplyr::mutate(diff_perc = (`N50 (Assembled)_new` - `N50 (Assembled)_old`) /
-                      `N50 (Assembled)_old`)
+      dplyr::select(.data$`Sample ID`, .data$`N50 (Assembled)_old`,
+                    .data$`N50 (Assembled)_new`) %>%
+      dplyr::mutate(diff_perc = (.data$`N50 (Assembled)_new` -
+                                   .data$`N50 (Assembled)_old`) /
+                      .data$`N50 (Assembled)_old`)
   })
   do.call("rbind", ass_list)
 }
@@ -115,14 +136,19 @@ combine_ehec_stats <- function(old, new) {
   comb <- oldEHEC %>%
     dplyr::inner_join(newEHEC, by = "Sample ID",  suffix = c("_old", "_new")) %>%
     dplyr::mutate(
-      Targets_Eval = sign(`Perc. Good Targets_old` - `Perc. Good Targets_new`),
-      ST_Eval = as.numeric(!is.na(`ST Warwick_old`) - !is.na(`ST Warwick_new`)),
-      CT_Eval = as.numeric(!is.na(`Complex Type_old`) -
-                             !is.na(`Complex Type_new`)),
-      HT_Eval = as.numeric(!is.na(`H Type_old`) - !is.na(`H Type_new`)),
-      OT_Eval = as.numeric(!is.na(`O Type_old`) - !is.na(`O Type_new`))
+      Targets_Eval = sign(.data$`Perc. Good Targets_old` -
+                            .data$`Perc. Good Targets_new`),
+      ST_Eval = as.numeric(!is.na(.data$`ST Warwick_old`) -
+                             !is.na(.data$`ST Warwick_new`)),
+      CT_Eval = as.numeric(!is.na(.data$`Complex Type_old`) -
+                             !is.na(.data$`Complex Type_new`)),
+      HT_Eval = as.numeric(!is.na(.data$`H Type_old`) -
+                             !is.na(.data$`H Type_new`)),
+      OT_Eval = as.numeric(!is.na(.data$`O Type_old`) -
+                             !is.na(.data$`O Type_new`))
     ) %>%
-   dplyr::select(`Sample ID`, Targets_Eval, ST_Eval, CT_Eval, HT_Eval, OT_Eval)
+   dplyr::select(.data$`Sample ID`, .data$Targets_Eval, .data$ST_Eval,
+                 .data$CT_Eval, .data$HT_Eval, .data$OT_Eval)
   comb
 }
 
@@ -141,13 +167,16 @@ combine_salmonella_stats <- function(old, new) {
   comb <- oldSal %>%
     dplyr::inner_join(newSal, by = "Sample ID",  suffix = c("_old", "_new")) %>%
     dplyr::mutate(
-      Targets_Eval = sign(`Perc. Good Targets_old` - `Perc. Good Targets_new`),
-      ST_Eval = as.numeric(!is.na(`ST_old`) - !is.na(`ST_new`)),
-      CT_Eval = as.numeric(!is.na(`Complex Type_old`) -
-                             !is.na(`Complex Type_new`)),
-      SV_Eval = as.numeric(!is.na(`Serovar_old`) - !is.na(`Serovar_new`))
+      Targets_Eval = sign(.data$`Perc. Good Targets_old` -
+                            .data$`Perc. Good Targets_new`),
+      ST_Eval = as.numeric(!is.na(.data$`ST_old`) - !is.na(.data$`ST_new`)),
+      CT_Eval = as.numeric(!is.na(.data$`Complex Type_old`) -
+                             !is.na(.data$`Complex Type_new`)),
+      SV_Eval = as.numeric(!is.na(.data$`Serovar_old`) -
+                             !is.na(.data$`Serovar_new`))
     ) %>%
-    dplyr::select(`Sample ID`, Targets_Eval, ST_Eval, CT_Eval, SV_Eval)
+    dplyr::select(.data$`Sample ID`, .data$Targets_Eval, .data$ST_Eval,
+                  .data$CT_Eval, .data$SV_Eval)
   comb
 }
 
@@ -170,33 +199,12 @@ combine_general_stats <- function(old, new, what) {
     dplyr::inner_join(newThis, by = "Sample ID",  suffix = c("_old", "_new")) %>%
     dplyr::mutate(
       Targets_Eval = sign(`Perc. Good Targets_old` - `Perc. Good Targets_new`),
-      ST_Eval = as.numeric(!is.na(`ST_old`) - !is.na(`ST_new`)),
-      CT_Eval = as.numeric(!is.na(`Complex Type_old`) -
-                             !is.na(`Complex Type_new`))
+      ST_Eval = as.numeric(!is.na(.data$`ST_old`) - !is.na(.data$`ST_new`)),
+      CT_Eval = as.numeric(!is.na(.data$`Complex Type_old`) -
+                             !is.na(.data$`Complex Type_new`))
     ) %>%
-    dplyr::select(`Sample ID`,
-                  Targets_Eval, ST_Eval, CT_Eval)
+    dplyr::select(.data$`Sample ID`,
+                  .data$Targets_Eval, .data$ST_Eval, .data$CT_Eval)
   comb
 }
 
-
-#
-# write_csv2(Assembly,
-#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Assembly.csv")
-#
-# write_csv2(EHEC,
-#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/EHEC.csv")
-#
-# write_csv2(Salmonella,
-#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Salmonella.csv")
-#
-# write_csv2(Listeria,
-#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Listeria.csv")
-#
-# write_csv2(MRSA,
-#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/MRSA.csv")
-#
-# write_csv2(Legionella,
-#            "O:/Abteilung Humanmedizin (AHM)/Referat 32/32_6/13_QM/Validierung Pipeline/Vergleiche/2025_03_19_VS_2025_03_20/Legionella.csv")
-#
-#
